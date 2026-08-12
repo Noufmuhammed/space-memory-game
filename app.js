f873d4b41064c82b79dc144b7fd0b912e0bb9f79
Nouf <noufm6992@gmail.com>
2026-08-12 11:55:22 +0300

    add card flip behavior

--- app.js
+++ app.js
@@ -19,18 +19,28 @@ function handleCardClick(event) {
   console.log("Card clicked")
   console.log(event.target)
   console.log(event.target.id)
-if (flippedCards.length === 2) {
-  return
-}
+
+  if (flippedCards.length === 2) {
+    return
+  }
 
   flippedCards.push(event.target)
   console.log(flippedCards)
 
+  event.target.style.display = "none"
+
   if (flippedCards.length === 2) {
     if (flippedCards[0].id === flippedCards[1].id) {
       console.log("Match!")
+      flippedCards = []
     } else {
       console.log("Not a match!")
+
+      setTimeout(function() {
+        flippedCards[0].style.display = "block"
+        flippedCards[1].style.display = "block"
+        flippedCards = []
+      }, 1000)
     }
   }
 }
