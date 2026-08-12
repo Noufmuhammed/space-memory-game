6e6ce7efdb5a1c6ba34e579329dc5cfaeadb3684
Nouf <noufm6992@gmail.com>
2026-08-12 11:05:43 +0300

    add card comparison

--- app.js
+++ app.js
@@ -18,8 +18,21 @@ console.log(cards)
 function handleCardClick(event) {
   console.log("Card clicked")
   console.log(event.target)
-  
+  console.log(event.target.id)
+if (flippedCards.length === 2) {
+  return
+}
 
+  flippedCards.push(event.target)
+  console.log(flippedCards)
+
+  if (flippedCards.length === 2) {
+    if (flippedCards[0].id === flippedCards[1].id) {
+      console.log("Match!")
+    } else {
+      console.log("Not a match!")
+    }
+  }
 }
 
 
